ed03fca1f40193abb1b00f44ffa83664f8853e34
Konst_V <varjutin@yandex.ru>
2022-04-06 13:37:43 +0300

    git init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+});
